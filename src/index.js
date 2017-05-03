43aaa741e95c670db49afc5b6748710bde7c76c2
mmsmsy <mmsmsy@gmail.com>
2017-05-03 16:33:15 +0200

    Added proper Routes in render and IndexRoute. Added high quality sprites for PokemonDetails, updated style.css, added more details to PokemonDetails, added prev,next buttons to PokemonDetails

--- src/index.js
+++ src/index.js
@@ -1,20 +1,60 @@
 import {render} from 'react-dom';
 import React, {Component} from 'react';
 import fetch from 'isomorphic-fetch';
-import { Router, Route, Link, hashHistory } from 'react-router';
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router';
 
 class PokemonDetails extends Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      pokemon: null,
+      errors: null
+    }
+  }
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+
+    fetch(`http://pokeapi.co/api/v2/pokemon/${this.props.params.id}`)
+      .then(res => res.json())
+      .then(pokemon => this.setState({
+        loading: false,
+        pokemon: pokemon
+      }))
+      .catch(errors => this.setState({
+        loading: false,
+        errors: errors
+      }))
+  }
   render(){
-    const {id} = this.state;
-    let content;
-    return <div className="pokemon--details">
-            <div className="pokemon--details--sprite">
-              <img src={`/public/sprites/${id}.png`} />
-            </div>
-            <p>
-              Name: {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
-            </p>
-          </div>
+    const {loading, pokemon, errors} = this.state;
+    if (loading || !pokemon) {
+      return (
+        <p className="pokemon--loading"> Loading ...</p>
+      )
+    }
+    const styles = {
+      'textTransform': 'capitalize'
+    }
+    return (
+      <div className="pokemon--details">
+        <Link className="back--to--list" to="/pokemon">Back to the list</Link>
+        <Link className="link--to--next" to={`/pokemon/${parseInt(this.props.params.id)+1}`}>Next</Link>
+        <Link className="link--to--prev" to={`/pokemon/${parseInt(this.props.params.id)-1}`}>Prev</Link>
+        <div className="pokemon--details--sprite">
+          <img src={`/public/spriteshq/${this.props.params.id}.png`} />
+        </div>
+        <table style={styles}>
+          <tbody><tr><td>Name</td><td>{pokemon.name}</td></tr></tbody>
+          <tbody><tr><td>Type</td><td>{pokemon.types[0].type.name}</td></tr></tbody>
+          <tbody><tr><td>Base experience</td><td>{pokemon.base_experience}</td></tr></tbody>
+          <tbody><tr><td>Weight</td><td>{pokemon.weight/10}kg</td></tr></tbody>
+          <tbody><tr><td>Height</td><td>{pokemon.height*10}cm</td></tr></tbody>
+        </table>
+      </div>
+    )
   }
 }
 
@@ -24,15 +64,22 @@ class PokemonDetails extends Component{
 class Pokemon extends Component{
   render(){
     const {pokemon,id} = this.props;
-    return <div className="pokemon--species">
-            <div className="pokemon--species--container">
-              <div className="pokemon--species--name"><ul><li><Link to={`/pokemon/${id}`}>{`${id}. ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}`}</Link></li></ul></div>
-              <div className="pokemon--species--sprite">
-                <img src={`/public/sprites/${id}.png`} />
-              </div>
-            </div>
-          </div>;
+    const styles = {
+      'textTransform': 'capitalize'
     }
+    return (
+      <div className="pokemon--species">
+        <div className="pokemon--species--container">
+          <Link to={`/pokemon/${id}`}>
+            <div className="pokemon--species--name" style={styles}>{`${id}. ${pokemon.name}`}</div>
+            <div className="pokemon--species--sprite">
+              <img src={`/public/sprites/${id}.png`} />
+            </div>
+          </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 
@@ -50,7 +97,7 @@ class PokemonList extends Component{
       loading : false,
     };
   }
-  componentWillMount(){
+  componentDidMount(){
     this.setState({
       loading : true
     });
@@ -68,16 +115,18 @@ class PokemonList extends Component{
     const {fetched, loading, species} = this.state;
     let content ;
     if(fetched){
-      content = <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
+      return <div className="pokemon--species--list">{species.map((pokemon,index)=><Pokemon key={pokemon.name} id={index+1} pokemon={pokemon}/>)}</div>;
     }else if(loading && !fetched){
-        content = <p className="pokemon--loading"> Loading ...</p>;
+        return <p className="pokemon--loading"> Loading ...</p>;
     }
     else{
-      content = <div/>;
+      return <div/>;
     }
-    return  <div>
-      {content}
-    </div>;
+    return (
+      <div>
+        {content}
+      </div>
+    )
   }
 }
 
@@ -85,17 +134,21 @@ class PokemonList extends Component{
 //This is the root component
 class PokeApp extends Component{
   render(){
-    return <div className="pokeapp">
-      <h1> Complete I-VI gen. PokeDex! (Including Mega)</h1>
-      <PokemonList/>
-    </div>;
+    return (
+      <div className="pokeapp">
+        <h1> Complete I-VI gen. PokeDex! (Including Mega)</h1>
+        {this.props.children}
+      </div>
+    )
   }
 }
 
 render((
   <Router history={hashHistory}>
     <Route path="/" component={PokeApp}>
-      <Route path={`/pokemon/${id}`} component={PokemonDetails} />
+      <IndexRoute component={PokemonList} />
+      <Route path="/pokemon" component={PokemonList} />
+      <Route path="/pokemon/:id" component={PokemonDetails}/>
     </Route>
   </Router>
 ),document.getElementById('app'))
